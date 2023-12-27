30f9dd1d758f9e333b277300def64a61fd0532b0
Jayavelu.S <104975781+Jayavelu2003@users.noreply.github.com>
2023-12-27 18:22:46 +0530

    Update Top Player in IPL.py
    To using web scraping python for IPL players lists

--- TOP 10 Player in IPL.py
+++ TOP 10 Player in IPL.py
@@ -1,19 +1,35 @@
-import requests
+from pydoc import text
 
+import requests
+from matplotlib import pyplot as plt
+from pandas import *
 from bs4 import BeautifulSoup
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 database=[]
 
-def Data(url):
+def Data(url, clean_currency=None):
     Collatecontent=requests.get(url=url).content
     soup=BeautifulSoup(Collatecontent,'html.parser')
 
     table = soup.find("table",{"class":"table scoretable"})
+
     th=table.find_all("th")
-    tr=table.find_all("td")
-    for data in th:
-        database.append(data.text)
-    for datas in tr:
-        database.append(datas.text)
-    print(database)
+    for i in th:
+        database.append(i.text)
+    df=DataFrame(columns=database)
+
+
+    body = table.find_all("tr")
+    for i in body[1:]:
+        player=i.find_all("td")
+        playerdata=[tr.text for tr in player]
+        #print(playerdata)
+        #len is a length
+        l=len(df)
+        #loc is a location
+        df.loc[l]=playerdata
+
+    print(df)
 Data(url="http://www.cricmetric.com/ipl/ranks/")
